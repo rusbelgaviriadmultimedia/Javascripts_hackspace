4e29c338f865d1dfd3a7de9b1825d85d098c4e61
Rusbel Gaviria <kimar041604@gmail.com>
2020-09-09 23:55:03 -0500

    Iniciando tutorial de conceptos básicos en https://youtu.be/tmRa0_EvMoc

--- 01 - JavaScript Drum Kit/playsound.js
+++ 01 - JavaScript Drum Kit/playsound.js
@@ -1,5 +1,14 @@
-function playsound(clave){
+/* *function playsound(clave){
     var sound_key=clave.getAttribute("data-key");
     alert("the" + clave.innerHTML + sound_key + ".");
 
-};
+};*/
+//window.addEventListener('type: DomString', callback: addEventListener, capture?: boolean)
+window.addEventListener('keydowm', function (e){
+    const audio = document.querySelector('audio[data-key=${e.keyCode}]');
+        if(!audio) return; //stop the function from running all together
+        audio.currentTime = 0;// rewind to the start
+        audio.play();
+        
+});
+
